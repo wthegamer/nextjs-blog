7c40a28c87151103df9fbb525bb4d636dc3b8dbc
Winston Young <winst1231@gmail.com>
2020-07-22 11:31:02 -0500

    Remove more boilerplate

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Winston'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Winston Young'
 
 export default function Layout({ children, home }) {
   return (
